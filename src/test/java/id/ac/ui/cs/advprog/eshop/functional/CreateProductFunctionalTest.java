5cbb65740afad373222be8c0b27e13525d6f8b9b
unknown <fini.ardiyanti@gmail.com>
2026-02-10 16:24:37 +0700

    Add functional test for Create Product

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
@@ -0,0 +1,49 @@
+package id.ac.ui.cs.advprog.eshop.functional;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ExtendWith(SeleniumJupiter.class)
+class CreateProductFunctionalTest {
+
+    @LocalServerPort
+    private int serverPort;
+
+    @Value("${app.baseUrl:http://localhost}")
+    private String testBaseUrl;
+
+    private String baseUrl;
+
+    @BeforeEach
+    void setupTest() {
+        baseUrl = String.format("%s:%d", testBaseUrl, serverPort);
+    }
+
+    @Test
+    void testCreateProduct(ChromeDriver driver) throws Exception {
+        driver.get(baseUrl + "/product/create");
+
+        driver.findElement(By.name("productName")).sendKeys("Sampo Cap Bango");
+        driver.findElement(By.name("productQuantity")).sendKeys("10");
+
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+
+         assertTrue(driver.getCurrentUrl().contains("/product/list"));
+
+        String pageSource = driver.getPageSource();
+        assertTrue(pageSource.contains("Sampo Cap Bango"));
+    }
+}
